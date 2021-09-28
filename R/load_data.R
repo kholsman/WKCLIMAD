f5319bb9d017ffbf0048bc4a05d96d5e46978dc3
K Holsman <kholsman@users.noreply.github.com>
2021-09-28 02:00:40 -0700

    updates pre wkshp sept

--- R/load_data.R
+++ R/load_data.R
@@ -3,5 +3,10 @@
 
 # Load base files:
 #----------------------
-aqua_dat <- readxl::read_xlsx(file.path(data.in,"Aquaculture_impacts_due_to_Clim2021-09-22_14_06_31.xlsx"))
-fish_dat <- readxl::read_xlsx(file.path(data.in,"Fisheries_impacts_due_to_Climat2021-09-22_14_06_01.xlsx"))
+aqua_dat <- readxl::read_xlsx(file.path(data.in,"Aquaculture_impacts_due_to_Clim2021-09-23_19_36_33.xlsx"))
+fish_dat <- readxl::read_xlsx(file.path(data.in,"Fisheries_impacts_due_to_Climat2021-09-23_19_35_33.xlsx"))
+
+fsh_lkup <- read.csv(file.path(data.in,"fishery_lkup.csv"))
+aqua_lkup <- read.csv(file.path(data.in,"aqua_lkup.csv"))
+
+                     
