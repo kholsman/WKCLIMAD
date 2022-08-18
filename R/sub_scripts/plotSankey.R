df2e2da0fc80fb50ccdb6b14c5d53af4a6ffd713
K Holsman <kholsman@users.noreply.github.com>
2022-08-18 16:20:23 -0700

    updates

--- R/sub_scripts/plotSankey.R
+++ R/sub_scripts/plotSankey.R
@@ -4,19 +4,27 @@ nm <-c("Fish_1_1",
        "Fish_1_2",
        "Fish_1_3",
        "Fish_2_1",
+       "Fish_2_2",
+       "Fish_2_3",
        "Aqua_1_1")
 
 
-nn <-1
+nn <-3
 
-
-nw <- read_model (fl  = "Data/in/flow diagrams/frame 8_aquacultre_1_1.xlsx",
-                       sheet_fl   = nm[nn],
-                       sizeIN     = 16, 
-                       NAval      = NA,
-                       lkup       = "meta_data")
-
-
+for(nn in 1:length(nm)){
+  nw <- suppressMessages( read_model (fl  = "Data/in/flow diagrams/Day5_ToolWebs.xlsx",
+                         sheet_fl   = nm[nn],
+                         sizeIN     = 16, 
+                         NAval      = NA,
+                         lkup       = "meta_data"))
+  if(!dir.exists("Data/out/snakeymodels")) dir.create("Data/out/snakeymodels")
+  
+  eval(parse(text= paste0(nm[nn],"<-nw")))
+  
+  eval(parse(text= paste0("save(",nm[nn],", file = 
+                                 file.path('Data/out/snakeymodels/',
+                                           paste0(nm[nn],'.Rdata')))")))
+}
 
 # # Make the Network
 p <- sankeyNetwork(Links = nw$links, Nodes = nw$nodes,
@@ -24,7 +32,7 @@ p <- sankeyNetwork(Links = nw$links, Nodes = nw$nodes,
                    Value = "value", NodeID = "label",
                    fontSize = 12, unit = "Letter(s)",
                   # nodeWidth = "weight_destination",
-                   NodeGroup  = 'Type',
+                   NodeGroup  = 'Category',
                    LinkGroup = "Type",
                   # LinkGroup = "Type",
                    sinksRight=FALSE)
